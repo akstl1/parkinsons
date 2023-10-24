5e15c272fd8f690efa4abdfbc973bc2e4f8e8502
Allan Khariton <allankhariton@gmail.com>
2023-10-23 21:49:20 -0400

    updated joblib

--- dsi-streamlit-web-app-parkinsons.py
+++ dsi-streamlit-web-app-parkinsons.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 
 # load our model pipeline object
-model = joblib.load("parkinsons_model.joblib")
+model = joblib.load("model.joblib")
 
 # insert a title for the app and instructions
 
